fa9903c2ecb79d7c99dd1e697d2d8a3ac6fe1833
Abdul _ Rehman Khokhar <sherrykhokhar295@gmail.com>
2023-10-15 20:03:15 +0500

    initial commit

---
+++ Whatsapp/script.js
@@ -0,0 +1,80 @@
+let msg1 = [];
+let msg2 = [];
+let wa1 = document.querySelector('#wa1'), wa2 = document.querySelector('#wa2');
+setInterval(() => {
+    let d = new Date();
+    let t = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+    document.querySelectorAll(".time")[0].innerHTML = `${t} `;
+    document.querySelectorAll(".time")[1].innerHTML = `${t}`;
+    document.querySelectorAll(".time")[2].innerHTML = `${t}`;
+    document.querySelectorAll(".time")[3].innerHTML = `${t}`;
+    let a = document.createElement("span");
+    a.innerHTML = `${msg1.length}`;
+    wa2.appendChild(a);
+    let b = document.createElement("span");
+    b.innerHTML = `${msg2.length}`;
+    wa1.appendChild(b);
+
+}, 0);
+let h1 = document.querySelector('#home1'), h2 = document.querySelector('#home2');
+let hB = document.querySelectorAll('.homeBtn');
+let app = document.querySelectorAll('.app');
+let apps = document.querySelectorAll('.apps');
+
+
+let inp1 = document.querySelector("#inp1");
+let inp2 = document.querySelector("#inp2");
+let m1 = document.querySelector("#m1");
+let m2 = document.querySelector("#m2");
+
+m1.addEventListener('click', () => {
+    msg1.push({ message: inp1.value })
+    const m = document.createElement('ul');
+    m.innerHTML = `<li>${inp1.value} <sub>${new Date().getMinutes()}.</sub></li>`;
+    apps[1].appendChild(m);
+    const n = document.createElement('ol');
+    n.innerHTML = `<li>${inp1.value} <sub>${new Date().getMinutes()}.</sub></li>`;
+    apps[0].appendChild(n);
+    inp1.value = null;
+})
+m2.addEventListener('click', () => {
+    msg2.push({ message: inp2.value })
+    const m = document.createElement('ul');
+    m.innerHTML = `<li>${inp2.value} <sub>${new Date().getMinutes()}.</sub></li>`;
+    apps[0].appendChild(m);
+    const n = document.createElement('ol');
+    n.innerHTML = `<li>${inp2.value} <sub>${new Date().getMinutes()}.</sub></li>`;
+    apps[1].appendChild(n);
+    inp2.value = null;
+})
+hB[0].addEventListener("click", () => {
+    h1.style.display = "flex";
+    app[0].style.display = "none";
+})
+hB[1].addEventListener("click", () => {
+    h2.style.display = "flex";
+    app[1].style.display = "none";
+})
+wa1.addEventListener("click", () => {
+    h1.style.display = "none";
+    app[0].style.display = "flex";
+    app[0].style.top = "0px";
+})
+wa2.addEventListener("click", () => {
+    h2.style.display = "none";
+    app[1].style.display = "flex";
+    app[1].style.top = "0px";
+})
+
+
+document.addEventListener('contextmenu', 
+     event => event.preventDefault()
+);
+document.addEventListener("keydown", function (event){
+    if (event.ctrlKey){
+       event.preventDefault();
+    }
+    if(event.keyCode == 123){
+       event.preventDefault();
+    }
+});

a767cf3f8436931d30c23418de2e9cb96b9859a3
Abdul _ Rehman Khokhar <sherrykhokhar295@gmail.com>
2023-10-19 15:01:58 +0500

    initial commit

--- ChatingApp/script.js
+++ ChatingApp/script.js
@@ -99,14 +99,14 @@ wa2.addEventListener("click", () => {
 })
 
 
-// document.addEventListener('contextmenu',
-//     event => event.preventDefault()
-// );
-// document.addEventListener("keydown", function (event) {
-//     if (event.ctrlKey) {
-//         event.preventDefault();
-//     }
-//     if (event.keyCode == 123) {
-//         event.preventDefault();
-//     }
-// });
+document.addEventListener('contextmenu',
+    event => event.preventDefault()
+);
+document.addEventListener("keydown", function (event) {
+    if (event.ctrlKey) {
+        event.preventDefault();
+    }
+    if (event.keyCode == 123) {
+        event.preventDefault();
+    }
+});
